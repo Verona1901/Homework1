589b6fda10ef5e69b7c808e0bd2390013dd0300a
Вероника Акулова <fenek1901@gmail.com>
2022-10-16 22:12:08 +0300

    Task29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -7,26 +7,27 @@
 int[] FillArray(int num)
 {
     int[] array = new int[num];
-    Console.Write("[");
+
     Random rnd = new Random();
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = new Random().Next(0,100);
+        array[i] = new Random().Next(0, 100);
     }
     return array;
 }
 void PrintArray(int[] arr)
 {
-    for (int i = 0; i < arr.Length-1; i++)
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
     {
-        Console.Write(arr[i]+ ", ");
-    }
-    {
+        Console.Write(arr[i] + ", ");
+
         if (arr.Length > 0)                 // чтобы запятая не ставилась после последнего элемента)
-        Console.Write(arr[arr.Length - 1]);  
+        Console.Write(arr[arr.Length - 1]);
     }
+    Console.Write("]");
 }
 int[] myArr = FillArray(8);
 PrintArray(myArr);
-Console.Write("]");
+
 
